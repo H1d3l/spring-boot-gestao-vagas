2d157474150fda7a0c4514a88983698ef8bb06b4
H1d3l <hidels.rios@gmail.com>
2023-10-25 10:26:02 -0300

    initial project

---
+++ src/main/java/br/com/hidelbrandorios/springbootgestaovagas/SpringBootGestaoVagasApplication.java
@@ -0,0 +1,13 @@
+package br.com.hidelbrandorios.springbootgestaovagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGestaoVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGestaoVagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hidelbrandorios/springbootgestaovagas/SpringBootGestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hidelbrandorios.springbootgestaovagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
